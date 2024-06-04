3e9b17c1c67a384e61c76cecacda5c76f34109a6
Kahvi-0xFF <46513413+Kahvi-0@users.noreply.github.com>
2024-06-04 01:36:18 -0400

    Create Framework.ps1

---
+++ Framework.ps1
@@ -0,0 +1,14 @@
+echo test
+function list-tools {
+   echo "--------------------------------"
+   echo "ALL TOOLS"
+   echo "--------------------------------"
+   echo "AVchecker"
+
+}
+
+function get-tool ($a) {
+    echo $a
+   
+
+}
