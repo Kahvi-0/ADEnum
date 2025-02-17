d6ee80ccf506d39f1de0ccb8920b4f38da34b835
Kahvi-0xFF <46513413+Kahvi-0@users.noreply.github.com>
2025-02-17 14:29:55 -0500

    Update ADEnum.ps1

--- ADEnum.ps1
+++ ADEnum.ps1
@@ -1,66 +1,66 @@
 function adenum {
     del log.txt -erroraction 'silentlycontinue'
-    del DomainUsers.txt -erroraction 'silentlycontinue'
-    Write-Host "=====[Domain Controllers]==========" -ForegroundColor Red | Tee-Object -file log.txt
-    #$Domain = "$env:userdnsdomain"
-    #nltest /dclist:$Domain
-    #Native LDAP
-    ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=8192))").findAll() | ForEach-Object { $_.properties.name}
-    Write-Host "=======[Domain Users]==========" -ForegroundColor Red | Tee-Object -file log.txt
-    (New-Object DirectoryServices.DirectorySearcher "objectcategory=user").FindAll() | ForEach-Object { $_.Properties.samaccountname } | Tee-Object -file log.txt 
-    Write-Host "=======[Domain Groups]==========" -ForegroundColor Red| Tee-Object -file log.txt
-    (New-Object DirectoryServices.DirectorySearcher "objectcategory=group").FindAll() | ForEach-Object { $_.Properties.samaccountname } | Tee-Object -file log.txt
-    Write-Host "=======[Enumerating Domain GPOs]==========" -ForegroundColor Red| Tee-Object -file log.txt
-    (New-Object DirectoryServices.DirectorySearcher "objectCategory=groupPolicyContainer").FindAll()| ForEach-Object { $_.Properties.displayname,$_.Properties.gpcfilesyspath,""}
-    Write-Host "=======[GPOs applied to current computer]==========" -ForegroundColor Red| Tee-Object -file log.txt
+    Write-Host "=====[Domain Controllers]==========" -ForegroundColor Red | Tee-Object -file AD-Status.log
+    ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=8192))").findAll() | ForEach-Object { $_.properties.name} | Tee-Object -file AD-Status.log
+    Write-Host "=======[Domain Users]==========" -ForegroundColor Red | Tee-Object -file AD-Status.log
+    (New-Object DirectoryServices.DirectorySearcher "objectcategory=user").FindAll() | ForEach-Object { $_.Properties.samaccountname } | Tee-Object -file AD-Status.log
+    Write-Host "=======[Domain Groups]==========" -ForegroundColor Red| Tee-Object -file AD-Status.log
+    (New-Object DirectoryServices.DirectorySearcher "objectcategory=group").FindAll() | ForEach-Object { $_.Properties.samaccountname } | Tee-Object -file AD-Status.log
+    Write-Host "=======[Members of the protected users group]==========" -ForegroundColor Red| Tee-Object -file AD-Status.log
+    ([adsisearcher]"(&(objectCategory=group)(name=protected users))").findAll() | ForEach-Object { $_.properties.name,$_.properties.member,""} | Tee-Object -file AD-Status.log
+    Write-Host "=======[Enumerating Domain GPOs]==========" -ForegroundColor Red| Tee-Object -file AD-Status.log
+    (New-Object DirectoryServices.DirectorySearcher "objectCategory=groupPolicyContainer").FindAll()| ForEach-Object { $_.Properties.displayname,$_.Properties.gpcfilesyspath,""} | Tee-Object -file AD-Status.log
+    Write-Host "=======[GPOs applied to current computer]==========" -ForegroundColor Red| Tee-Object -file AD-Status.log
     #Get-DomainGPO -ComputerIdentity (hostname) | select displayname,gpcfilesyspath,objectcategory | format-list | Tee-Object -file log.txt
-    Write-Host "=======[GPOs applied to current user]==========" -ForegroundColor Red| Tee-Object -file log.txt   
-    $GUID = [guid]::New(([adsisearcher]"SamAccountName=$env:USERNAME").FindOne().Properties.objectguid[0]).Guid
-    echo "Still figuring a clean way to convert GUIDS"
-    ([adsisearcher]"(&(objectCategory=groupPolicyContainer))").findAll() | ForEach-Object { $_.properties.displayname,$_.properties.gpcmachineextensionnames,""}
+    Write-Host "=======[GPOs applied to current user]==========" -ForegroundColor Red| Tee-Object -file AD-Status.log
+    $GUID = [guid]::New(([adsisearcher]"SamAccountName=$env:USERNAME").FindOne().Properties.objectguid[0]).Guid | Tee-Object -file AD-Status.log
+    echo "Still figuring a clean way to convert GUIDS" | Tee-Object -file AD-Status.log
+    ([adsisearcher]"(&(objectCategory=groupPolicyContainer))").findAll() | ForEach-Object { $_.properties.displayname,$_.properties.gpcmachineextensionnames,""} | Tee-Object -file AD-Status.log
 
     #$user =[Environment]::UserName
     #([adsisearcher]"(&(objectCategory=user)(name=$user))").findAll()
     #[guid]::New(([adsisearcher]"SamAccountName=$env:COMPUTERNAME`$").FindOne().Properties.objectguid[0]).Guid
     #[guid]::New(([adsisearcher]"SamAccountName=$env:USERNAME").FindOne().Properties.objectguid[0]).Guid
-    Write-Host "=======[Enumerating LDAP descriptions]==========" -ForegroundColor Red| Tee-Object -file log.txt
-    ([adsisearcher]"(&(objectCategory=user)(description=*))").findAll() | ForEach-Object { $_.properties.name,$_.properties.description,""}
-    Write-Host "=======[Enumerating current user's MAQ]==========" -ForegroundColor Red| Tee-Object -file log.txt 
-    echo "MAQ:"
-    (New-Object DirectoryServices.DirectorySearcher "ms-DS-MachineAccountQuota=*").FindAll() | ForEach-Object { $_.Properties.'ms-ds-machineaccountquota'} | Tee-Object -file log.txt 
-    Write-Host "=======[Domain Trusts]==========" -ForegroundColor Red| Tee-Object -file log.txt 
-    nltest /DOMAIN_TRUSTS /ALL_TRUSTS
-    Write-Host "=======[Kerberoastable Users]==========" -ForegroundColor Red| Tee-Object -file log.txt 
-    ([adsisearcher]"(&(objectCategory=user)(servicePrincipalname=*))").findAll() | ForEach-Object { $_.properties.name,$_.properties.serviceprincipalname,""} | Tee-Object -file log.txt
-    Write-Host "=======[ASREP roastable Users]==========" -ForegroundColor Red| Tee-Object -file log.txt 
-    ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=4194304))").findAll() | ForEach-Object { $_.properties.name} | Tee-Object -file log.txt
-    Write-Host "=======[ADCS]==========" -ForegroundColor Red| Tee-Object -file log.txt 
-    Write-Host "To Do, for now use NXC or another tool <3" -ForegroundColor Green| Tee-Object -file log.txt
-    Write-Host "=======[LDAP Signing]==========" -ForegroundColor Red| Tee-Object -file log.txt 
-    Write-Host "To Do, for now use NXC or another tool <3" -ForegroundColor Green| Tee-Object -file log.txt
-    Write-Host "=======[Unconstrained Delegation]==========" -ForegroundColor Red| Tee-Object -file log.txt 
-    ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=524288))").findAll() | ForEach-Object { $_.properties.name}
-    Write-Host "=======[Constrained Delegation]==========" -ForegroundColor Red| Tee-Object -file log.txt 
-    ([adsisearcher]"(&(msds-allowedtodelegateto=*))").findAll() | ForEach-Object { $_.properties.name,$_.properties."msds-allowedtodelegateto",""}
-    Write-Host "=======[Accounts marked for No Delegation]==========" -ForegroundColor Red| Tee-Object -file log.txt 
-    ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=1048576))").findAll() | ForEach-Object { $_.properties.name}
-    Write-Host "=======[Accounts that require smart cards for interaction]==========" -ForegroundColor Red| Tee-Object -file log.txt
-    ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=262144))").findAll() | ForEach-Object { $_.properties.name}
-    Write-Host "=======[Accounts where a password is not required]==========" -ForegroundColor Red| Tee-Object -file log.txt
-    ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=32))").findAll() | ForEach-Object { $_.properties.name}
-    Write-Host "=======[Interdomain Trust: Accounts trusted for a system domain that trusts other domains.]==========" -ForegroundColor Red| Tee-Object -file log.txt
-    ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=2048))").findAll() | ForEach-Object { $_.properties.name}
-    Write-Host "=======[GMSA Service]==========" -ForegroundColor Red| Tee-Object -file log.txt 
-    echo "Need to expand on later"
-    ([adsisearcher]"(&(objectClass=msDS-ManagedServiceAccount))").findAll() | ForEach-Object { $_.properties,""}
-    ([adsisearcher]"(&(PrincipalsAllowedToRetrieveManagedPassword=*))").findAll() | ForEach-Object { $_.properties,""}
-    Write-Host "=======[LAPS]==========" -ForegroundColor Red| Tee-Object -file log.txt 
-    ([adsisearcher]"(&(objectCategory=computer)(ms-MCS-AdmPwd=*)(sAMAccountName=*))").findAll() | ForEach-Object { $_.properties} | Tee-Object -file log.txt
-    Write-Host "=======[SCCM]==========" -ForegroundColor Red| Tee-Object -file log.txt
-    ([ADSISearcher]("objectClass=mSSMSManagementPoint")).FindAll() | % {$_.Properties} | Tee-Object -file log.txt
-    Write-Host "=======[MSSQL]==========" -ForegroundColor Red| Tee-Object -file log.txt
+    Write-Host "=======[Enumerating LDAP descriptions]==========" -ForegroundColor Red| Tee-Object -file AD-Status.log
+    ([adsisearcher]"(&(objectCategory=user)(description=*))").findAll() | ForEach-Object { $_.properties.name,$_.properties.description,""} | Tee-Object -file AD-Status.log
+    Write-Host "=======[Enumerating current user's MAQ]==========" -ForegroundColor Red| Tee-Object -file AD-Status.log
+    echo "MAQ:" | Tee-Object -file AD-Status.log
+    (New-Object DirectoryServices.DirectorySearcher "ms-DS-MachineAccountQuota=*").FindAll() | ForEach-Object { $_.Properties.'ms-ds-machineaccountquota'} | Tee-Object -file AD-Status.log
+    Write-Host "=======[Domain Trusts]==========" -ForegroundColor Red| Tee-Object -file AD-Status.log
+    nltest /DOMAIN_TRUSTS /ALL_TRUSTS | Tee-Object -file AD-Status.log
+    Write-Host "=======[Kerberoastable Users]==========" -ForegroundColor Red| Tee-Object -file AD-Status.log
+    ([adsisearcher]"(&(objectCategory=user)(servicePrincipalname=*))").findAll() | ForEach-Object { $_.properties.name,$_.properties.serviceprincipalname,""} | Tee-Object -file AD-Status.log
+    Write-Host "=======[ASREP roastable Users]==========" -ForegroundColor Red| Tee-Object -file AD-Status.log
+    ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=4194304))").findAll() | ForEach-Object { $_.properties.name} | Tee-Object -file AD-Status.log
+    Write-Host "=======[ADCS]==========" -ForegroundColor Red| Tee-Object -file AD-Status.log
+    Write-Host "To Do, for now use NXC or another tool <3" -ForegroundColor Green| Tee-Object -file AD-Status.log
+    Write-Host "=======[LDAP Signing]==========" -ForegroundColor Red| Tee-Object -file AD-Status.log
+    Write-Host "To Do, for now use NXC or another tool <3" -ForegroundColor Green| Tee-Object -file AD-Status.log
+    Write-Host "=======[Unconstrained Delegation]==========" -ForegroundColor Red| Tee-Object -file AD-Status.log
+    ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=524288))").findAll() | ForEach-Object { $_.properties.name} | Tee-Object -file AD-Status.log
+    Write-Host "=======[Constrained Delegation]==========" -ForegroundColor Red| Tee-Object -file AD-Status.log
+    ([adsisearcher]"(&(msds-allowedtodelegateto=*))").findAll() | ForEach-Object { $_.properties.name,$_.properties."msds-allowedtodelegateto",""} | Tee-Object -file AD-Status.log
+    Write-Host "=======[Accounts marked for No Delegation]==========" -ForegroundColor Red| Tee-Object -file AD-Status.log
+    ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=1048576))").findAll() | ForEach-Object { $_.properties.name} | Tee-Object -file AD-Status.log
+    Write-Host "=======[Accounts that require smart cards for interaction]==========" -ForegroundColor Red| Tee-Object -file AD-Status.log
+    ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=262144))").findAll() | ForEach-Object { $_.properties.name} | Tee-Object -file AD-Status.log
+    Write-Host "=======[Accounts where a password is not required]==========" -ForegroundColor Red| Tee-Object -file AD-Status.log
+    ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=32))").findAll() | ForEach-Object { $_.properties.name} | Tee-Object -file AD-Status.log
+    Write-Host "=======[Interdomain Trust: Accounts trusted for a system domain that trusts other domains.]==========" -ForegroundColor Red| Tee-Object -file AD-Status.log
+    ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=2048))").findAll() | ForEach-Object { $_.properties.name} | Tee-Object -file AD-Status.log
+    Write-Host "=======[GMSA Service]==========" -ForegroundColor Red| Tee-Object -file AD-Status.log
+    echo "Need to expand on later" | Tee-Object -file AD-Status.log
+    ([adsisearcher]"(&(objectClass=msDS-ManagedServiceAccount))").findAll() | ForEach-Object { $_.properties,""} | Tee-Object -file AD-Status.log
+    ([adsisearcher]"(&(PrincipalsAllowedToRetrieveManagedPassword=*))").findAll() | ForEach-Object { $_.properties,""} | Tee-Object -file AD-Status.log
+    Write-Host "=======[LAPS]==========" -ForegroundColor Red| Tee-Object -file AD-Status.log
+    ([adsisearcher]"(&(objectCategory=computer)(ms-MCS-AdmPwd=*)(sAMAccountName=*))").findAll() | ForEach-Object { $_.properties} | Tee-Object -file AD-Status.log
+    Write-Host "=======[SCCM]==========" -ForegroundColor Red| Tee-Object -file AD-Status.log
+    ([ADSISearcher]("objectClass=mSSMSManagementPoint")).FindAll() | % {$_.Properties} | Tee-Object -file AD-Status.log
+    Write-Host "=======[MSSQL]==========" -ForegroundColor Red| Tee-Object -file AD-Status.log
     echo "Not perfect, computer accounts based off name. Sill enum via nmap with -sV"
-    ([adsisearcher]"(&(objectCategory=computer)(Name=*SQL*))").findAll() | ForEach-Object { $_.properties.name,""}
+    ([adsisearcher]"(&(objectCategory=computer)(Name=*SQL*))").findAll() | ForEach-Object { $_.properties.name,""} | Tee-Object -file AD-Status.log
 
-
+    Write-Host "================================" -ForegroundColor Red
+    Write-Host "Output is saved in AD-Status.log" -ForegroundColor Green
+    Write-Host "================================" -ForegroundColor Red
 }
