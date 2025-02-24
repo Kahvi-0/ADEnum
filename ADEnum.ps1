0c664d6f999a69a640a701589a7c1bae494b918d
Kahvi-0xFF <46513413+Kahvi-0@users.noreply.github.com>
2025-02-24 10:34:25 -0500

    Update ADEnum.ps1

--- ADEnum.ps1
+++ ADEnum.ps1
@@ -10,22 +10,31 @@ function adenum {
     Write-Host "=======[Domain Trusts]==========" -BackgroundColor Red
     nltest /DOMAIN_TRUSTS /ALL_TRUSTS 
     Write-Host "=======[Domain Users]==========" -BackgroundColor Red 
-    (New-Object DirectoryServices.DirectorySearcher "objectcategory=user").FindAll() | ForEach-Object { $_.Properties.samaccountname } 
+    (New-Object DirectoryServices.DirectorySearcher "objectcategory=user").FindAll() | ForEach-Object { $_.Properties.samaccountname }
     Write-Host "=======[Domain Groups]==========" -BackgroundColor Red
     (New-Object DirectoryServices.DirectorySearcher "objectcategory=group").FindAll() | ForEach-Object { $_.Properties.samaccountname } 
     Write-Host "=======[Members of the protected users group]==========" -BackgroundColor Red
     ([adsisearcher]"(&(objectCategory=group)(name=protected users))").findAll() | ForEach-Object { $_.properties.name,$_.properties.member,""} 
+    Write-Host "=======[Accounts marked for No Delegation]==========" -BackgroundColor Red
+    ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=1048576))").findAll() | ForEach-Object { $_.properties.name} 
+    Write-Host "=======[Accounts that require smart cards for interaction]==========" -BackgroundColor Red
+    ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=262144))").findAll() | ForEach-Object { $_.properties.name} 
+    Write-Host "=======[Accounts where a password is not required]==========" -BackgroundColor Red
+    ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=32))").findAll() | ForEach-Object { $_.properties.name} 
+    Write-Host "=======[Interdomain Trust: Accounts trusted for a system domain that trusts other domains.]==========" -BackgroundColor Red
+    ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=2048))").findAll() | ForEach-Object { $_.properties.name} 
+    Write-Host "=======[Enumerating LDAP descriptions]==========" -BackgroundColor Red
+    ([adsisearcher]"(&(objectCategory=user)(description=*))").findAll() | ForEach-Object { $_.properties.name,$_.properties.description,""} 
+    Write-Host "=======[Enumerating current user's MAQ]==========" -BackgroundColor Red
+    Write-Host "Number of computer accounts that your account can create" -ForegroundColor Green
+    echo "MAQ:" 
+    (New-Object DirectoryServices.DirectorySearcher "ms-DS-MachineAccountQuota=*").FindAll() | ForEach-Object { $_.Properties.'ms-ds-machineaccountquota'} 
     Write-Host "=======[Enumerating Domain GPOs]==========" -BackgroundColor Red
     (New-Object DirectoryServices.DirectorySearcher "objectCategory=groupPolicyContainer").FindAll()| ForEach-Object { $_.Properties.displayname,$_.Properties.gpcfilesyspath,""} 
     Write-Host "=======[GPOs applied to current user and computer]==========" -BackgroundColor Red
     Write-Host "If you do not see the computer settings, elevate powershell to admin" -ForegroundColor Green
     gpresult /r /f
     Write-Host ""
-    Write-Host "=======[Enumerating LDAP descriptions]==========" -BackgroundColor Red
-    ([adsisearcher]"(&(objectCategory=user)(description=*))").findAll() | ForEach-Object { $_.properties.name,$_.properties.description,""} 
-    Write-Host "=======[Enumerating current user's MAQ]==========" -BackgroundColor Red
-    echo "MAQ:" 
-    (New-Object DirectoryServices.DirectorySearcher "ms-DS-MachineAccountQuota=*").FindAll() | ForEach-Object { $_.Properties.'ms-ds-machineaccountquota'} 
     Write-Host "=======[Enumerate dangerous user attributes (not exhaustive)==========" -BackgroundColor Red
     Write-Host "Need to look into the format of each, belive its in UTF-8 format" -ForegroundColor Green
     Write-Host "Users with the 'userPassword' attribute" -ForegroundColor Green
@@ -43,28 +52,24 @@ function adenum {
     Write-Host "Users with the 'dBCSPwd' attribute" -ForegroundColor Green
     ([adsisearcher]"(&(dBCSPwd=*))").findAll() | ForEach-Object { $_.properties.name,$_.properties."dbcspwd",""} 
     Write-Host "=======[Kerberoastable Users]==========" -BackgroundColor Red
+    Write-Host "Rubeus.exe kerberoast /format:hashcat /nowrap" -Backgroundcolor magenta
     ([adsisearcher]"(&(objectCategory=user)(servicePrincipalname=*))").findAll() | ForEach-Object { $_.properties.name,$_.properties.serviceprincipalname,""} 
     Write-Host "=======[ASREP roastable Users]==========" -BackgroundColor Red
+    Write-Host "Rubeus.exe asreproast /format:hashcat" -Backgroundcolor magenta
     ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=4194304))").findAll() | ForEach-Object { $_.properties.name} 
     Write-Host "=======[ADCS]==========" -BackgroundColor Red
+    Write-Host "Enumerate ADCS servers. Run with further tools" -ForegroundColor Green
+    Write-Host "Certify.exe find /vulnerable" -Backgroundcolor magenta
     $Root = [adsi] "LDAP://CN=Configuration,$baseDN"
     $Searcher = new-object System.DirectoryServices.DirectorySearcher($root)
     $Searcher.filter = "(&(objectClass=pKIEnrollmentService))"
     $Searcher.FindAll() | ForEach-Object { "Hostname:", $_.properties.dnshostname,  "CA name:",$_.properties.displayname,  "Entrollment endpoints:", $_.properties."mspki-enrollment-servers", $_.properties."certificatetemplates", "" }
     Write-Host "=======[LDAP Signing]==========" -BackgroundColor Red
-    Write-Host "To Do, for now use NXC or another tool <3" -ForegroundColor Green
-    Write-Host "=======[Unconstrained Delegation]==========" -BackgroundColor Red
+    Write-Host "To Do, for now use NXC or another tool <3" -ForegroundColor Yellow
+    Write-Host "=======[Unconstrained Delegation hosts]==========" -BackgroundColor Red
     ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=524288))").findAll() | ForEach-Object { $_.properties.name} 
-    Write-Host "=======[Constrained Delegation]==========" -BackgroundColor Red
+    Write-Host "=======[Constrained Delegation hosts]==========" -BackgroundColor Red
     ([adsisearcher]"(&(msds-allowedtodelegateto=*))").findAll() | ForEach-Object { $_.properties.name,$_.properties."msds-allowedtodelegateto",""} 
-    Write-Host "=======[Accounts marked for No Delegation]==========" -BackgroundColor Red
-    ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=1048576))").findAll() | ForEach-Object { $_.properties.name} 
-    Write-Host "=======[Accounts that require smart cards for interaction]==========" -BackgroundColor Red
-    ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=262144))").findAll() | ForEach-Object { $_.properties.name} 
-    Write-Host "=======[Accounts where a password is not required]==========" -BackgroundColor Red
-    ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=32))").findAll() | ForEach-Object { $_.properties.name} 
-    Write-Host "=======[Interdomain Trust: Accounts trusted for a system domain that trusts other domains.]==========" -BackgroundColor Red
-    ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=2048))").findAll() | ForEach-Object { $_.properties.name} 
     Write-Host "=======[GMSA Service]==========" -BackgroundColor Red
     Write-Host "Need to expand on later" -ForegroundColor Green
     ([adsisearcher]"(&(objectClass=msDS-ManagedServiceAccount))").findAll() | ForEach-Object { $_.properties,""} 
@@ -72,14 +77,21 @@ function adenum {
     Write-Host "=======[LAPS]==========" -BackgroundColor Red
     ([adsisearcher]"(&(objectCategory=computer)(ms-MCS-AdmPwd=*)(sAMAccountName=*))").findAll() | ForEach-Object { $_.properties}  
     Write-Host "=======[SCCM]==========" -BackgroundColor Red
+    Write-Host "SharpSCCM.exe local site-info --no-banner" -Backgroundcolor magenta
     ([ADSISearcher]("objectClass=mSSMSManagementPoint")).FindAll() | % {$_.Properties} 
     Write-Host "=======[MSSQL]==========" -BackgroundColor Red
-    Write-Host "Not perfect, computer accounts based off name. Sill enum via nmap with -sV" -ForegroundColor Green
-    ([adsisearcher]"(&(objectCategory=computer)(Name=*SQL*))").findAll() | ForEach-Object { $_.properties.name,""} 
-    Write-Host "=======[DNS Dynamic Update Settings]==========" -BackgroundColor Red
+    Write-Host "Not perfect, computer accounts based off name" -ForegroundColor Green
+    Write-Host "Sill enum via nmap with -sV" -Backgroundcolor magenta
+    ([adsisearcher]"(&(objectCategory=computer)(Name=*SQL*))").findAll() | ForEach-Object { $_.properties.name,""}
+
+    Write-Host "=======[Permissions for DNS]==========" -BackgroundColor Red
+    Write-Host "Still in the works - cannot pinpoint the privs for each zone yet https://i.kym-cdn.com/entries/icons/original/000/041/998/Screen_Shot_2022-09-23_at_10.40.58_AM.jpg" -ForegroundColor Yellow
+    Write-Host ""
+    Write-Host "DNS Dynamic Update Settings" -BackgroundColor Green
     Write-Host "Still in the works - cannot pinpoint exact zone via LDAP yet" -ForegroundColor Yellow
-    Write-Host "Look for Nonsecure and Secure, this means Anynoymous DNS updates." -ForegroundColor Green
+    Write-Host "To attempt to update your own host's IP to test:" -ForegroundColor Green
     Write-Host "python3 ./adddns.py --domain lab.local --dnsip [ADIDNS Server ip] --hostname [target hostname] --hostip [IP address to modify]" -Backgroundcolor magenta
+    Write-Host "Look for Nonsecure and Secure, this means Anynoymous DNS updates." -ForegroundColor Green
     $dnsZones = ([ADSISearcher]("(&(objectClass=dnsZone))")).FindAll()
     foreach ($zone in $dnsZones) {
         $properties = $zone.Properties
@@ -107,9 +119,8 @@ function adenum {
         }
     }
     
-    Write-Host "=======[Permissions for DNS]==========" -BackgroundColor Red
-    Write-Host "Still in the works - cannot pinpoint the privs for each zone yet https://i.kym-cdn.com/entries/icons/original/000/041/998/Screen_Shot_2022-09-23_at_10.40.58_AM.jpg" -ForegroundColor Yellow
-    Write-Host "=======[Check for low priv users/groups with privs to update DNS]==========" -ForegroundColor Green
+
+    Write-Host "Low priv users/groups with privs to update DNS" -ForegroundColor Green
     Write-Host "python3 ./dnstool.py -r 'UpdateTest' -a add --data 10.10.10.68 -u '' -p '' [DC IP]" -Backgroundcolor magenta
     $searchRoot = "LDAP://CN=MicrosoftDNS,CN=System,$baseDN"
     $searcher = New-Object DirectoryServices.DirectorySearcher
@@ -166,18 +177,16 @@ function adenum {
 
     #Password policy enumeration
     #uses the first DC returned.
-    $DC = ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=8192))").findOne() | ForEach-Object { $_.properties.name}
     echo ""
     Write-Host "=======[Checking password policy, GPOs, and fine grain policies from: $DC]==========" -BackgroundColor Red
     echo ""
+    $DC = ([adsisearcher]"(&(userAccountControl:1.2.840.113556.1.4.803:=8192))").findOne() | ForEach-Object { $_.properties.name}
     echo "Checking policy applied to current account" -ForegroundColor Green
     net accounts
-
     echo ""
     Write-Host "Checking other policies" -ForegroundColor Green
     echo ""
     Get-ChildItem \\$DC\sysvol\*\GptTmpl.inf -Recurse -erroraction 'silentlycontinue'  | select-string -Pattern ".*Bad.*|Password.*"  -AllMatches |  Format-Table -GroupBy Path -Property line
-
     echo ""
     Write-Host "Checking for accounts with a fine grain policy applied" -ForegroundColor Green
     echo ""
